894081367efe398d24d7c86983227d2a8f79033b
Teresa Cho <tcho@cca.edu>
2019-12-31 14:36:57 -0800

    adds gameboard component to app.js module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { render } from '@testing-library/react';
+// Must import components used in JSX
+import GameBoard from './components/GameBoard/GameBoard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">React Mastermind</header>
+        <GameBoard />
       </div>
     );
   }
