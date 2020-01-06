436cfe1709d1b1d9c10423669cd2188e9cde8cce
Teresa Cho <tcho@cca.edu>
2020-01-06 15:22:05 -0800

    creates a guesspeg component

--- src/components/GuessRow/GuessRow.jsx
+++ src/components/GuessRow/GuessRow.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GuessPegs from '../GuessPegs/GuessPegs';
+import GuessScore from '../GuessScore/GuessScore';
 
 const GuessRow = (props) => (
   <div className='component'>
     <div>
       Guess Row #
       <GuessPegs />
+      <GuessScore />
     </div>
   </div>
 );
