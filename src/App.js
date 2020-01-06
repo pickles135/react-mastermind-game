a039eb170295f314c158ab127d241396c3643099
Teresa Cho <tcho@cca.edu>
2020-01-06 15:03:02 -0800

    imports NewGameButton component to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import GameBoard from './components/GameBoard/GameBoard';
 import ColorPicker from './components/ColorPicker/ColorPicker';
 import GameTimer from './components/GameTimer/GameTimer'
+import NewGameButton from './components/NewGameButton/NewGameButton';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <GameBoard />
         <ColorPicker />
         <GameTimer />
+        <NewGameButton />
       </div>
     );
   }
