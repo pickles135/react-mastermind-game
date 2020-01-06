836d9f1f94bfe1073e7152a69fdc8d4a201502c5
Teresa Cho <tcho@cca.edu>
2020-01-06 15:14:04 -0800

    imports GuessRow component into gameboard component

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import GuessRow from '../GuessRow/GuessRow'
 
 const GameBoard = (props) => (
   <div className='component'>
     <div> 
       GameBoard
+      <GuessRow />
+      <GuessRow />
     </div>
   </div>
 );
