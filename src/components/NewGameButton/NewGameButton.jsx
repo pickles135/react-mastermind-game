110a07a7c15722eedd01adfc3cf527d7c955c27f
Teresa Cho <tcho@cca.edu>
2020-01-06 14:58:15 -0800

    creates NewGameButton component w/ basic bpolerplate

---
+++ src/components/NewGameButton/NewGameButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewGameButton = (props) => (
+  <div className='components'>
+    <div>
+      NewGameButton
+    </div>
+  </div>
+);
+
+export default NewGameButton;
