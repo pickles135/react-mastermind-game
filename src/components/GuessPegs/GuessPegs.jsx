acfaa78fc85fab8a7d581d429bbd1684dcaeb659
Teresa Cho <tcho@cca.edu>
2020-01-06 15:24:30 -0800

    imports guesspeg into guesspegs

--- src/components/GuessPegs/GuessPegs.jsx
+++ src/components/GuessPegs/GuessPegs.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import GuessPeg from '../GuessPeg/GuessPeg'
 
 const GuessPegs = (props) => (
   <div className='component'>
     <div>
       GuessPegs
+      <GuessPeg />
+      <GuessPeg />
+      <GuessPeg />
+      <GuessPeg />
     </div>
   </div>
 );
