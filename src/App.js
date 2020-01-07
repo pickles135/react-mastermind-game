088ac9670eaa7b1b1b1778eb7d9cd8704290830e
Teresa Cho <tcho@cca.edu>
2020-01-06 22:42:48 -0800

    refractors the app page w/ footer component taken away, also adds display flex to the components for styling

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import GameBoard from './components/GameBoard/GameBoard';
 import ColorPicker from './components/ColorPicker/ColorPicker';
 import GameTimer from './components/GameTimer/GameTimer'
 import NewGameButton from './components/NewGameButton/NewGameButton';
-import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">React Mastermind</header>
-        <GameBoard />
-        <ColorPicker />
-        <GameTimer />
-        <NewGameButton />
-        <Footer />
+        <div className='flex-h'>
+          <GameBoard />
+          <div>
+            <ColorPicker />
+            <GameTimer />
+            <NewGameButton />
+          </div>
+        </div>
+        <footer>footer</footer>
       </div>
     );
   }
