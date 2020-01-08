83d701c93307922e6db4ca0570ab9bbf8e9e4992
Teresa Cho <tcho@cca.edu>
2020-01-08 14:49:00 -0800

    passes props to color picker

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
             <div className='flex-h'>
               <GameBoard />
               <div>
-                <ColorPicker />
+                <ColorPicker colors={colors} />
                 <GameTimer />
                 <NewGameButton />
               </div>
